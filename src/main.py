3d10dbd9dc122c178be4e380c1d7c26e50200669
jjvargass <jotavargas89@gmail.com>
2019-08-19 09:44:32 -0500

    pruebas unitarias y requerimientos de pip

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
-from flask import Flask, escape, request
-
+from flask import Flask
 app = Flask(__name__)
 
+def suma(a,b):
+    return a+b
+
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+def hello_world():
+    resultado = suma(3,2)
+    return "Hello, World! %s" % (resultado)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
